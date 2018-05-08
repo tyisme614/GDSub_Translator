a6300205f93a083d2630d7e1a82b4e78045be53e
Yuan <yuant614@gmail.com>
2018-05-09 00:28:19 +0800

    test google cloud speech api

--- app.js
+++ app.js
@@ -2,38 +2,79 @@ var lbl = require('linebyline');
 var fs = require('fs');
 //google cloud translate api
 var google_translate = require('@google-cloud/translate');
+//google cloud speech api
+var google_speech = require('@google-cloud/speech');
+
+
 var projectID = 'GLocalizationProjects';
+
 var translator = new google_translate({
 	projectId: projectID,
 	keyFilename: '/home/yuan/auth/GLocalizationProjects-4f795dcb895a.json'
 });
+
+
 var target_lang = 'zh';
 
+var speech = new google_speech.SpeechClient();
+var src_audio = 'test.pcm';
+//loading audio file
+console.log('loading audio file');
+var audio_base64 = fs.readFileSync(src_audio).toString('base64');
+const audio = {
+	content: audio_base64
+}
 
+const config = {
+  encoding: 'LINEAR16',
+  sampleRateHertz: 16000,
+  languageCode: 'en-US',
+  enableAutomaticPunctuation: true
+};
+const request = {
+  audio: audio,
+  config: config,
+};
+ 
+// Detects speech in the audio file
+console.log('start recognizing speech');
+client
+  .recognize(request)
+  .then(data => {
+    const response = data[0];
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n');
+    console.log(`Transcription: ${transcription}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
 // var file = 'test.srt';
 // var target = 'output.txt';
 
-const EventEmitter = require('events');
-class customEventEmitter extends EventEmitter{};
-const stateEmitter = new customEventEmitter();
-var index =0;
-stateEmitter.on('next', function(){
-	index++;
-	if(index < buffer.length){
-		console.log('next word: ' + buffer[index]);
-		txt_arr += buffer[index] + ' ';
-		translateSubtitle(txt_arr);
-	}else{
-		console.log('translation done.');
-	}
-});
+// const EventEmitter = require('events');
+// class customEventEmitter extends EventEmitter{};
+// const stateEmitter = new customEventEmitter();
+// var index =0;
+// stateEmitter.on('next', function(){
+// 	index++;
+// 	if(index < buffer.length){
+// 		console.log('next word: ' + buffer[index]);
+// 		txt_arr += buffer[index] + ' ';
+// 		translateSubtitle(txt_arr);
+// 	}else{
+// 		console.log('translation done.');
+// 	}
+// });
 
 //var process = lbl(__dirname + '/sentences.txt');
-var process = lbl(__dirname + '/src.txt');
+// var process = lbl(__dirname + '/src.txt');
 //var process = lbl(src);
-var buffer = [];
-var txt_arr = '';
-var count = 0;
+// var buffer = [];
+// var txt_arr = '';
+// var count = 0;
+
 //creating output file
 // console.log('creating output file...');
 // fs.closeSync(fs.openSync(target, 'w'));
@@ -69,31 +110,33 @@ var count = 0;
 // 		});
 // 	}
 // });
-console.log('reading source file');
-process.on('line', function(data, lineCount, byteCount){
-	console.log(data);
-	//word by word
-	 buffer = data.split(' ');
-	//sentence by sentence
-	//buffer.push(data);
 
 
-})
-.on('error', (err) => {
-	console.log(err);
-})
-.on('close', (err) => {
-	if(err)
-		console.log(err);
-	console.log('starting translating...');
-	// traverseBuffer(buffer);
-	index = 0;
-	//word by word
-	 txt_arr += buffer[index] + ' ';
-	//sentence by sentence
-	//txt_arr = buffer[index];
-	translateSubtitle(txt_arr);
-});
+// console.log('reading source file');
+// process.on('line', function(data, lineCount, byteCount){
+// 	console.log(data);
+// 	//word by word
+// 	 buffer = data.split(' ');
+// 	//sentence by sentence
+// 	//buffer.push(data);
+
+
+// })
+// .on('error', (err) => {
+// 	console.log(err);
+// })
+// .on('close', (err) => {
+// 	if(err)
+// 		console.log(err);
+// 	console.log('starting translating...');
+// 	// traverseBuffer(buffer);
+// 	index = 0;
+// 	//word by word
+// 	 txt_arr += buffer[index] + ' ';
+// 	//sentence by sentence
+// 	//txt_arr = buffer[index];
+// 	translateSubtitle(txt_arr);
+// });
 
 function traverseBuffer(b){
 	for(var i=0; i<b.length; i++){
